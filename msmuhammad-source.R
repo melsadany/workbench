f805d22c0f52ed418ffdfd0966832418af2b0c75
Muhammad Elsadany <melsadany24@gmail.com>
2025-06-05 08:06:48 -0500

    updated my R source script

--- msmuhammad-source.R
+++ msmuhammad-source.R
@@ -2,7 +2,7 @@
 #                            main source file for Muhammad                         #
 ####################################################################################
 
-device <- ifelse(grepl("/LSS/", system("cd &pwd", intern = T)), "IDAS", "argon")
+device <- ifelse(any(grepl("LSS", system("ls ~", intern = T))), "IDAS", "argon")
 source(paste0(ifelse(device == "IDAS", "~/LSS", "/Dedicated"),
               "/jmichaelson-wdata/msmuhammad/workbench/customized-functions/correct_path.R"))
 
@@ -68,7 +68,8 @@ theme_set(theme_minimal() +
                   title = element_text(face = "bold", size = 10),
                   plot.caption = element_text(hjust = 0)
             ))
-my.guides <- guides(fill = guide_colorbar(barwidth = 6, barheight = 0.5))
+my.guides <- guides(fill = guide_colorbar(barwidth = 6, barheight = 0.5),
+                    color = guide_colorbar(barwidth = 6, barheight = 0.5))
 poster_theme <- theme_minimal() +
   theme(axis.line = element_line(colour = "black", size = 0.5),
         axis.text = element_text(face = "bold"),
@@ -99,6 +100,7 @@ bw.theme <- theme_linedraw() +
 boxplot.colors <- c("#aaf0d1", "#b39eb5")
 build.directory <- "mkdir -p archive; mkdir -p logs; mkdir -p figs"
 redblu.col <-  c("#ff6961", "#89cff0")
+redblu.col.2 <- c("#ff4600","#4782b4")
 redblack.col <- c("#800000", "black")
 six.colors <- c("#800000", "#cc7277", "#4f6162", "#e65236", "#56483a", "#73937e")
 ten.colors <- c("#800000", "#cc7277", "#4f6162", "#e65236", "#56483a", 
@@ -106,6 +108,11 @@ ten.colors <- c("#800000", "#cc7277", "#4f6162", "#e65236", "#56483a",
 antique.colors <- c("#855C75","#D9AF6B","#AF6458","#736F4C","#526A83",
                     "#625377","#68855C","#9C9C5E","#A06177","#8C785D",
                     "#467378","#7C7C7C")
+redblu.ni.col <- c("#E43339", "#0033FF")
+warm.cold.col <- c("blue", "deepskyblue", "white", "yellow", "orange", "red")
+abstract.colors <- c("#CC0A7D", "#EAA91E", "#A46CE1", "#7B8CB2", "#798632", "#4A9A3B", "#FFD9B8", "#781285", "#C746FF", "#F9FF32")
+abstract.colors.2 <- c("#86324A", "#333286", "#798632", "#4A9A3B", "#643264", "#3B39D2", "#DCD813", "#00FF00", "#DD9FDD", "#2D8B57", "#86CDEB")
+density.colors <- c("#0d0887", "#6a00a8", "#b12a90", "#e16462", "#fca636", "#f0f921")
 ####################################################################################
 # correct_path <- function(file) {
 #   device <- ifelse(grepl("/LSS/", system("cd &pwd", intern = T)), "IDAS", "argon")
@@ -223,8 +230,9 @@ corr.table <- function(x, y, method = "pearson") {
   return(p.ready)
 }
 # ggplot tiles
-redblu.col.gradient <- scale_fill_gradient2(low = redblu.col[2], high = redblu.col[1], name ="ρ")
-redblack.col.gradient <- scale_fill_gradient2(low = redblack.col[2], high = redblack.col[1], name ="ρ")
+redblu.col.gradient <- function(label="ρ"){scale_fill_gradient2(low = redblu.col[2], high = redblu.col[1], name =label)}
+redblu.col.gradient.2 <- function(label="ρ"){scale_fill_gradient2(low = redblu.col.2[2], high = redblu.col.2[1], name =label)}
+redblack.col.gradient <- function(label="ρ"){scale_fill_gradient2(low = redblack.col[2], high = redblack.col[1], name =label)}
 null_labs <- labs(x="",y="")
 ####################################################################################
 rho <- "ρ"
@@ -320,6 +328,88 @@ estimate.plot <- function(df = df) {
     labs(x="Estimate", y="")
 }
 
+####################################################################################
+archetypes_summ <- function(obj, k, points_labels) {
+  ss <- simplexplot(obj)
+  arc <- rbind(cbind(ss$proj_z, 
+                     text = paste0("A", c(1:k)))) %>% 
+    as.data.frame()
+  a.df <- cbind(ss$proj_h,
+                text = points_labels) %>%
+    rbind(cbind(ss$proj_z, 
+                text = paste0("A", c(1:k)))) %>%
+    as.data.frame() %>%
+    mutate(x=as.numeric(x),
+           y=as.numeric(y),
+           lab = ifelse(text %in% paste0("A", c(1:k)), T, F),
+           size2 = ifelse(grepl("A", text), 1, 0.7)) %>%
+    full_join(arc %>% complete(nesting(x,y), text) %>% 
+                select(text, xend=x, yend=y) %>%
+                left_join(arc, .,by = 'text') %>%
+                filter(!(x==xend & y==yend)) %>%
+                mutate_at(.vars = c(1,2,4,5), 
+                          .funs = function(x) as.numeric(x)) %>% 
+                select(-text) %>% 
+                mutate(lab = F)) %>%
+    mutate(text = ifelse(is.na(xend), text, ""))
+  
+  alpha.mat <- coef(obj)
+  arch.mat <- parameters(obj)
+  b.df <- as.matrix(alpha.mat) %*% as.matrix(arch.mat)
+  
+  ret <- list(simplex_plot = a.df,
+              arch_coef = alpha.mat,
+              arch.weights = arch.mat,
+              reduced_data = b.df)
+  return(ret)
+}
+####################################################################################
+####################################################################################
+remove_outliers_iqr <- function(df, column) {
+  Q1 <- quantile(df[[column]], 0.25, na.rm = TRUE)
+  Q3 <- quantile(df[[column]], 0.75, na.rm = TRUE)
+  IQR_value <- Q3 - Q1
+  
+  lower_bound <- Q1 - 1.5 * IQR_value
+  upper_bound <- Q3 + 1.5 * IQR_value
+  
+  df_filtered <- df[df[[column]] >= lower_bound & df[[column]] <= upper_bound, ]
+  return(df_filtered)
+}
+
+####################################################################################
+####################################################################################
+coefs_table <- function(model, lmer = F) {
+  require(jtools)
+  if(!lmer) {
+    jtools::summ(model, pval = T, confin=T)$coeftable %>%
+      as.data.frame() %>%
+      rownames_to_column("x") %>%
+      rename("Estimate" = `Est.`,
+             confin_min = `2.5%`,
+             confin_max = `97.5%`,
+             pval = `p`)
+  } else if(lmer) {
+    summary(model)$coefficients %>% as.data.frame() %>%
+      rownames_to_column("x") %>% 
+      rename(Estimate = 2, pval = 6) %>% 
+      inner_join(as.data.frame(confint(model)) %>% rownames_to_column("x") %>% 
+                   rename(confin_min=2,confin_max=3)) %>%
+      select(x,Estimate, pval, confin_min,confin_max)
+  }
+}
+####################################################################################
+####################################################################################
+fisher_table <- function(x, y) {
+  tt <- fisher.test(x = x, y = y)
+  data.frame(OR = as.numeric(tt$estimate),
+             pval = as.numeric(tt$p.value),
+             conf_min = as.numeric(tt$conf.int[1]),
+             conf_max = as.numeric(tt$conf.int[2]))
+}
+
+####################################################################################
+####################################################################################
 ####################################################################################
 ####################################################################################
 ####################################################################################
